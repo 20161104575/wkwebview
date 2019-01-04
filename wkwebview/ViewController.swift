10b142fb165d46d5bf2e2ef26c8b061d96f6b80e
20161104575 <1975950726@qq.com>
2019-01-04 15:16:31 +0800

    webview
    webview

--- wkwebview/ViewController.swift
+++ wkwebview/ViewController.swift
@@ -7,14 +7,46 @@
 //
 
 import UIKit
-
+import WebKit
 class ViewController: UIViewController {
 
+    @IBOutlet weak var textfile: UITextField!
+    @IBOutlet weak var webview: WKWebView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
-
+    
+    @IBAction func backclick(_ sender: Any) {
+        webview.goBack()
+    }
+    
+    @IBAction func toclick(_ sender: Any) {
+        webview.goForward()
+    }
+    
+    @IBAction func new(_ sender: Any) {
+        webview.reload()
+    }
+    @IBAction func search(_ sender: Any) {
+        let urls = textfile.text
+        webview.load(NSURLRequest(url: NSURL(string: urls!)! as URL)as URLRequest)
+    }
+    @IBAction func textEndOnExit(_ sender: Any){
+        textfile.resignFirstResponder()
+    }
+    
+    @IBAction func touchUpInside(_ sender: UIControl){
+        textfile.resignFirstResponder()
+    }
+    
+    override func touchesEnded(_ touches: Set<UITouch>,with event: UIEvent?){
+        textfile.resignFirstResponder()
+    }
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    
 }
 
